8c599f902a4836b2a0457c7607c146f6ad4b9bd0
ranasaha <rana.saha@sap.com>
2018-05-17 14:28:13 +0800

    First Commit

---
+++ HelloWorldApp/howdy/views.py
@@ -0,0 +1,12 @@
+ # howdy/views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
+
+class AboutPageView(TemplateView):
+    def get(self, request, *kwargs):
+        return render(request, 'about.html', context=None)
---
+++ my_ve/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/i346261/Documents/git/personal/django-first-project/my_ve/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
